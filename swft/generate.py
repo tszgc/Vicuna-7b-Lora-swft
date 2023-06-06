7a752dc4f462756c6c6628eb28bdf88de1a42e87
zhaogc <zhaogc@fusionskye.com>
2023-06-06 11:03:16 +0800

    19

--- swft/generate.py
+++ swft/generate.py
@@ -144,6 +144,7 @@ def evaluate(
 if __name__ == "__main__":
     print('开始测试1')
     input = "###-TASK-A-A-A, no matter feasibility, answer only one word, 'positive' or 'negative', by this sentence:Blockware\u2019s team expects Bitcoin\u2019s adoption rate to be faster than previous technologies, but believes it's still in early-stage growth.\\xa0"
+    input = "介绍一下毛泽东"
     prompt = input
     # input = "no matter feasibility, answer only one word, 'positive' or 'negative', by this sentence:Blockware\u2019s team expects Bitcoin\u2019s adoption rate to be faster than previous technologies, but believes it's still in early-stage growth.\\xa0"
 
